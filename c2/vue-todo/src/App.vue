7afc8c23ae7e9650dcb37e2c6aeb2be7b134ab46
Tanvir Hasan <47018365+virza805@users.noreply.github.com>
2022-02-21 16:16:22 +0600

    Class 3 done

--- c2/vue-todo/src/App.vue
+++ c2/vue-todo/src/App.vue
@@ -1,83 +1,68 @@
 <template>
-  <div class="container mx-auto px-4">
-    <Header />
-    <div class="flex justify-center -space-x-20 my-5">
-      <div class="mix-blend-multiply h-40 w-40 bg-blue-400 rounded-full"></div>
-      <div class="mix-blend-multiply h-40 w-40 bg-pink-400 rounded-full"></div>
-    </div>
-    <h1 class="text-center text-4xl border-b-4 pb-4 border-indigo-700 ">Tailwind CSS ToDos</h1>
+
+  <Layout class="container mx-auto px-4">
+   
+   <template #app-title> Vue.js Todos </template>
+    
     <div class="component">
-      <form action="" @submit.prevent="addTodo">
-        <input v-model="newTodo" type="text" placeholder="Type and hit enter..." class=" bg-gray-200 w-full p-4">
-      </form>
+      <Composer v-model="newTodo" @onEnter="addTodo" />
       <div class="filter flex justify-between">
         <div class="filter-left flex justify-start items-center">
-          <button class="filter-btn px-3 text-xl">All ({{todoCount}}) </button>
-          <button class="filter-btn px-3 text-xl">Pending ({{ undoneCount }}) </button>
-          <button class="filter-btn px-3 text-xl">Done ({{ doneCount }}) </button>
+          <button @click="filter_mode = 'all'" class="filter-btn px-3 text-xl">All ({{todoCount}}) </button>
+          <button @click="filter_mode = 'undone'" class="filter-btn px-3 text-xl">Pending ({{ undoneCount }}) </button>
+          <button @click="filter_mode = 'done'" class="filter-btn px-3 text-xl">Done ({{ doneCount }}) </button>
         </div>
-        <button class="filter-btn text-red-800 px-3 text-xl">Clear </button>
+        <button class="filter-btn text-red-800 px-3 text-xl" @click= "clear">Clear </button>
 
       </div>
 
       <!-- ToDo Start Now -->
-      <Todo v-for="todo in todos" :key="todo.id" :todo="todo" @onToggle="toggleDone" />
-      <!-- <div
-       v-for="todo in todos" :key="todo.id" 
-       :class="{ 'border-green-600': todo.done, }"
-       @click="toggleDone(todo)"
-       class="todo p-3 my-3 flex items-center border-l-4 cursor-pointer ">
-        <div 
-        :class="{ 'bg-green-600': todo.done, }"
-        class="todo-status-dot h-2 w-2 bg-yellow-600 rounded-full mr-3"></div>
-        <div class=" ">
-          <p class="todo-content text-lg font-medium">{{ todo.task}}</p>
-          <small class="todo-time">{{ todo.time }}</small>
-        </div>
-      </div> -->
-      <!-- The End ToDo -->
+      <Todo 
+      v-for="todo in filteredTodos" 
+      :key="todo.id" 
+      :todo="todo" 
+      @onToggle="toggleDone" 
+      
+      />
       
 
     </div>
-  </div>
+  </Layout>
 </template>
 
 <script>
   import {v4 as uid} from 'uuid'
   import Header from './components/Header.vue'
 import Todo from './components/Todo.vue'
+import Composer from './components/Composer.vue'
+import Layout from './components/Layout.vue'
   export default {
-    name: "Tanvir",
+    name: "TodoApp",
+    components: {
+      Header,
+        Todo,
+        Composer,
+        Layout
+    },
+
+    mounted() {
+      this.todos = JSON.parse(localStorage.getItem("todos"));
+    },
+    updated() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+
     data() {
       return {
         newTodo: "",
-        todos: [
-          {
-            id: 1,
-            task: "Learn vue.js",
-            done: false,
-            time: "Wed 14 Feb 2022 21:19:25 GMT +06 (Bangladesh)",
-          },
-          {
-            id: 2,
-            task: "Learn Laravel",
-            done: true,
-            time: "Wed 14 Feb 2022 21:19:25 GMT +06 (Bangladesh)",
-          },
-          {
-            id: 3,
-            task: "Learn laravel with vue.js",
-            done: false,
-            time: "Wed 14 Feb 2022 21:19:25 GMT +06 (Bangladesh)",
-          },
-        ]
-      }
-    },
-    components: {
-      Header,
-        Todo
+        todos: [],
+        filter_mode: "all",
+      };
     },
     methods: {
+      handleTyping(task){
+        this.newTodo = task;
+      },
       addTodo(e) {
         const todo = {
         // alert("adding todo")
@@ -92,9 +77,19 @@ import Todo from './components/Todo.vue'
       },
       toggleDone(todo) {
         todo.done = !todo.done;
+      },
+      clear() {
+        this.todos = this.todos.filter((todo) => !todo.done);
       }
     },
     computed: {
+      filteredTodos(){
+        if(this.filter_mode == "all") return this.todos;
+        if(this.filter_mode == "done") return this.todos.filter((todo) => todo.done);
+        if(this.filter_mode == "undone") return this.todos.filter((todo) => !todo.done);
+      },
+
+
       todoCount() {
         return this.todos.length;
       },
--- c2/vue-todo/src/components/Layout.vue
+++ c2/vue-todo/src/components/Layout.vue
@@ -1,36 +1,27 @@
-<template lang="">
-    <div>
-        <header class="flex text-center align-item place-items-center">
-            <img class=" w-16" src="../assets/logo.png" alt="">
-            <ul class="flex justify-center items-center space-x-1">
-                <li class="px-2"><a href="/">Home</a></li>
-                <li class="px-2"><a href="">About</a></li>
-                <li class="px-2"><a href="">Contact</a></li>
-            </ul>
-        </header>
-        <section class="flex bg-purple-700">
-            <aside class=" w-1/5 ">Left Side bar</aside>
-            <main class=" bg-gray-300 w-3/5" >
-                <h2>Title</h2>
-                <HelloWorld name="Tanvir" age="28" />
-                <HelloWorld name="Hasan" age="25" />
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam vero est, rerum suscipit ab magni impedit ullam itaque aspernatur recusandae fuga possimus at voluptas nulla eos sapiente nam exercitationem odio.
-            </main>
-            <aside class=" w-1/5 ">
-                right side bar
-            </aside>
-        </section>
-        <footer class="bg-gray-900 text-center text-yellow-500 py-2">Copy right 2022</footer>
-    </div>
+<template>
+  <div class="app-container">
+      <Header />
+      <div class="flex justify-center -space-x-20 my-5">
+        <div class="mix-blend-multiply h-40 w-40 bg-blue-400 rounded-full"></div>
+        <div class="mix-blend-multiply h-40 w-40 bg-pink-400 rounded-full"></div>
+      </div>
+    <h1 class="text-center text-4xl border-b-4 pb-4 border-indigo-700 ">
+        <slot name="app-title">
+        Tailwind CSS ToDos
+        </slot>
+    </h1>
+      <slot />
+  </div>
 </template>
+
 <script>
-import HelloWorld from './HelloWorld.vue'
+import Header from './Header.vue'
 export default {
-    name: "Layout",
-    components: { HelloWorld },
-    
+  components: { Header },
+
 }
 </script>
-<style lang="">
-    
+
+<style>
+
 </style>

22646ec69c1391cae492dd642c177d043d327317
MD AL AMIN <01mdalamin1@gmail.com>
2022-02-23 08:32:04 +0600

    Vue 3 Bootcamp  Class 4 Start

---
+++ c4/c4.php
@@ -0,0 +1,14 @@
+<?php 
+
+#  => Vue Storage <= Vue 3 + Vite => #
+
+
+# npm init vite@latest => 
+# cd vuestorage => 
+# npm install => 
+# npm run dev => 
+# npm install postcss -D => 
+# npm install -D tailwindcss postcss autoprefixer => https://tailwindcss.com/docs/installation/using-postcss
+# npx tailwindcss init -p => 
+#  => 
+

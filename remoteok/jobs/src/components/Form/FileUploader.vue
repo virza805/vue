68c40791fbbff06c115a08d96489aae755060431
MD AL AMIN <01mdalamin1@gmail.com>
2022-04-01 09:53:12 +0600

    img upload by FilePond done

--- remoteok/jobs/src/components/Form/FileUploader.vue
+++ remoteok/jobs/src/components/Form/FileUploader.vue
@@ -1,66 +1,60 @@
 <template>
-  <div >
-    <div v-if="modelValue" class="flex items-center justify-between border border-slate-600">
-        <div class="flex items-center">
-            <img class="w-12 h-12 " :src="modelValue" alt="Company Logo">
-            <p>Company Logo</p>
-        </div>
-
-        <button 
-        @click="handleDeleteImage(modelValue)"  
-        class="pr-4 text-2xl text-red-500">&times;</button>
+  <div class="flex items-center justify-between border border-slate-600" v-if="modelValue" >
+    <div class="flex items-center">
+      <img class="w-12 h-12" :src="modelValue" alt="company logo" />
+      <p class="ml-2 pl-2">Company Logo</p>
     </div>
-    <FilePond v-else :server="serverConfig" name="file" />
+
+    <button
+      class="pr-4 text-2xl text-red-500"
+      @click="handleDeleteImage(modelValue)"
+    >
+      &times;
+    </button>
   </div>
+
+  <FilePond v-else :server="serverConfig" name="file" />
 </template>
 
 <script setup>
-// Import Vue FilePond
 import vueFilePond from "vue-filepond";
-
 import "filepond/dist/filepond.min.css";
 import api from "@/helpers/api";
 
- defineProps: ({
-    modelValue: {
-        
+defineProps({
+  modelValue: {
     default: "",
     type: String,
-    },
+  },
 });
-
 
 const emit = defineEmits(["update:modelValue"]);
 
-// Create component
 const FilePond = vueFilePond();
 
-
 const serverConfig = {
-    process: {
-        url: import.meta.env.VITE_API_URL + "/api/uploads",
-        // url: "https://jobs-api.return0.codes/api/uploads",
-        method: "POST",
-        headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-        withCredentials: false,
-        onload: (response) => {
-            // console.log(JSON.parse(response).url);
-            emit("update:modelValue", JSON.parse(response).url);
-        },
+  process: {
+    // url: import.meta.env.VITE_API_URL + "/api/uploads",
+    url: "https://jobs-api.return0.codes/api/uploads",
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("token"),
     },
+    withCredentials: false,
+    onload: (response) => {
+      emit("update:modelValue", JSON.parse(response).url);
+    },
+  },
 };
 
 const handleDeleteImage = async (url) => {
-    if(!confirm("Sure to delete ?")) return;
+  if (!confirm("Sure to delete?")) return;
 
-    emit("update:modelValue", "");
-    const { ok } = await api.delete(`/api/uploads?url=${url}`);
+  emit("update:modelValue", "");
+  const { ok } = await api.delete(`/api/uploads?url=${url}`);
 
-    if (!ok) {
-        emit("update:modelValue", url);
-    }
+  if (!ok) {
+    emit("update:modelValue", url);
+  }
 };
-
 </script>
--- remoteok/jobs/src/helpers/api.js
+++ remoteok/jobs/src/helpers/api.js
@@ -1,8 +1,8 @@
 import { create } from "apisauce";
 
 const api = create({
-    // baseURL: "https://jobs-api.return0.codes",
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://jobs-api.return0.codes",
+    // baseURL: import.meta.env.VITE_API_URL,
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
